35b17f991b46a34b9accd978a79222dd612b0de4
Sean Ryan <piefayth@gmail.com>
2023-09-12 15:48:08 -0500

    support miners generating addresses/wallets

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { loadSync } from "https://deno.land/std@0.199.0/dotenv/mod.ts";
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 import { mine } from "./mine.ts";
-import { newWallet } from "./wallet.ts";
+import { minerWallet, poolWallet } from "./wallet.ts";
 
 if (!import.meta.main) {
   console.error("main.ts is not a module")
@@ -17,18 +17,25 @@ const mineCommand = new Command()
       await mine(poolUrl)
   })
 
-const newWalletCommand = new Command()
+const poolWalletCommand = new Command()
   .description("Create a new pool operator wallet.")
   .option("-p, --preview", "Use testnet")
   .action(({ preview }) => {
-    newWallet(preview)
+    poolWallet(preview)
   })
 
+const minerWalletCommand = new Command()
+  .description("Create a new miner wallet.")
+  .option("-p, --preview", "Use testnet")
+  .action(({ preview }) => {
+    minerWallet(preview)
+  })
 
 await new Command()
   .name("tunapond")
   .description("A pool-aware miner submission interface for Fortuna.")
   .version("0.0.1")
   .command("mine", mineCommand)
-  .command("new_wallet", newWalletCommand)
+  .command("pool_wallet", poolWalletCommand)
+  .command("mining_wallet", minerWalletCommand)
   .parse(Deno.args);
--- wallet.ts
+++ wallet.ts
@@ -1,32 +1,51 @@
-import { C, generatePrivateKey } from "https://deno.land/x/lucid@0.10.1/mod.ts";
+import { C, Lucid, generatePrivateKey, generateSeedPhrase } from "https://deno.land/x/lucid@0.10.1/mod.ts";
 
-export function newWallet(isPreview = false) {
+export async function minerWallet(isPreview = false) {
+    const network = isPreview ? "Preview" : "Mainnet"
+    
+    let seed: string | undefined = undefined
     try {
-      const miningKeyRaw = Deno.readTextFileSync("miningKey.sk")
-      console.log("Keys already exists, not generating new key...")
-  
-      const privateMiningKey = C.PrivateKey.from_bech32(miningKeyRaw)
-  
-      const publicMiningKey = privateMiningKey.to_public();
-  
-      console.log(
-        `Mining Address: ${publicKeyToEnterpriseAddress(publicMiningKey, isPreview)}`
-      )
-    } catch {
-      const miningKeyRaw = generatePrivateKey()
-      const privateMiningKey = C.PrivateKey.from_bech32(miningKeyRaw)
-      const publicMiningKey = privateMiningKey.to_public();
-  
-      console.log(
-        `Mining Address: ${publicKeyToEnterpriseAddress(publicMiningKey, isPreview)}`
-      )
-  
-      Deno.writeTextFileSync("miningKey.sk", miningKeyRaw);
+        seed = Deno.readTextFileSync("seed.txt")
+        console.log("Mining key already exists, not generating new key...")
+    } catch (e) {
+        seed = generateSeedPhrase();
+        Deno.writeTextFileSync("seed.txt", seed);
     }
-  }
-  
-  function publicKeyToEnterpriseAddress(publicKey: C.PublicKey, isPreview: boolean) {
+
+    const lucid = await Lucid.new(undefined, network)
+    lucid.selectWalletFromSeed(seed)
+    console.log(`Miner wallet initialized and saved to seed.txt`)
+    console.log(await lucid.wallet.address())
+}
+
+export function poolWallet(isPreview = false) {
+    try {
+        const miningKeyRaw = Deno.readTextFileSync("poolKey.sk")
+        console.log("Pool key already exists, not generating new key...")
+
+        const privateMiningKey = C.PrivateKey.from_bech32(miningKeyRaw)
+
+        const publicMiningKey = privateMiningKey.to_public();
+
+        console.log(
+            `Pool Address: ${publicKeyToEnterpriseAddress(publicMiningKey, isPreview)}`
+        )
+    } catch {
+        const miningKeyRaw = generatePrivateKey()
+        const privateMiningKey = C.PrivateKey.from_bech32(miningKeyRaw)
+        const publicMiningKey = privateMiningKey.to_public();
+
+        console.log(
+            `Pool Address: ${publicKeyToEnterpriseAddress(publicMiningKey, isPreview)}`
+        )
+
+        Deno.writeTextFileSync("poolKey.sk", miningKeyRaw);
+    }
+}
+
+function publicKeyToEnterpriseAddress(publicKey: C.PublicKey, isPreview: boolean) {
     const cred = C.StakeCredential.from_keyhash(publicKey.hash());
     const enterprise = C.EnterpriseAddress.new(isPreview ? 0 : 1, cred);
     return enterprise.to_address().to_bech32(isPreview ? "addr_test" : "addr")
-  }
+}
+
